46064118921550f01ac8d0d6295d394f76c77b2c
vizdec <victormrodrguez06@gmail.com>
2024-05-03 15:58:49 -0500

    'portUpdate'

--- part3/telephoneAgencyBackend/index.js
+++ part3/telephoneAgencyBackend/index.js
@@ -87,5 +87,7 @@ app.post('/api/persons', (req, res) => {
   res.json(person)
 })
 
-const PORT = 3001
-app.listen(PORT)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
