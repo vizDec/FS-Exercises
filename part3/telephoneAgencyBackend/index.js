8917865b267d558aad16c8106276e8f9ab0693bf
vizdec <victormrodrguez06@gmail.com>
2024-05-10 12:31:19 -0500

    FullStackApp

--- part3/telephoneAgencyBackend/index.js
+++ part3/telephoneAgencyBackend/index.js
@@ -7,6 +7,7 @@ morgan.token('postData', (req, res) => {
   return JSON.stringify(req.body)
 })
 
+app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms - :postData"))
